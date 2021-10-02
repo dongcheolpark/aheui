e65fad5d7a173ef903dad07c853059bd3f012c24
dongcheolpark <head02325@soongsil.ac.kr>
2021-10-02 19:53:07 +0900

    a

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # aheui
 아희 인터프리터
+
+## 참조
+>https://rachelsdotcom.tistory.com/79
--- test.c
+++ test.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
 #define MAX 1000
 
+typedef short han;
+
+//<header>
+
 bool help(int argc,char * argv[]);
+
+han ** transfer_han(char *);
 
 int main(int argc,char * argv[]) {
 
@@ -17,11 +24,36 @@ int main(int argc,char * argv[]) {
 	char buffer[MAX] = { 0, };
 
 	fread(buffer,1,MAX,fp);
-	printf("%s",buffer);
+
+	han ** data = transfer_han(buffer);
 
 	fclose(fp);
-
 	return 0;
+}
+
+han ** transfer_han(char * buffer) {
+	han ** data;
+	int enter_couter=0;
+	int width = 0,height = 0;
+	int buffer_size = 0;
+	for(int i = 0;i<MAX;i++) {
+		if(buffer[i] == EOF) {
+			buffer_size = i;
+			break;
+		}
+		else if(buffer[i] == 0x0A) {
+			enter_couter++;
+			width = i;
+		}
+	}
+	height = enter_couter+1;
+	data = (han**)malloc(sizeof(han*) * (height));
+	for(int i = 0;i<height;i++) {
+		data[i] = (han *)malloc(sizeof(han) * (width));
+	}
+	for(int i = 0;i<buffer_size;i++) {
+
+	}
 }
 
 bool help(int argc,char * argv[]) {
